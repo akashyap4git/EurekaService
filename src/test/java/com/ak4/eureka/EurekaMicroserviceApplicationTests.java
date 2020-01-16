5f44049897203cdd2f791b1179202230e7cc2322
ambarishk <ambarishk@juniper.net>
2020-01-16 18:20:39 +0530

    Eureka Microservice First Draft

---
+++ src/test/java/com/ak4/eureka/EurekaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ak4.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
